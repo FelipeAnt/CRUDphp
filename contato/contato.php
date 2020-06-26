ad28ce565cf399bd847dd21c61fde5ef4b2d6f82
FelipeAnt <62069338+FelipeAnt@users.noreply.github.com>
2020-06-26 15:04:29 -0300

    Add files via upload
    page contato

---
+++ contato/contato.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CONTATOS</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap-4.3.1.css" rel="stylesheet">
+    <link href="stily.css" rel="stylesheet" type="text/css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+       <a class="navbar-brand" href="../index.php"><i class="fa fa-reply" aria-hidden="true"></i> RETORNAR</a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+       </button>
+    </nav>
+	  <section>
+		  <div class="wave wave1"></div>
+		  <div class="wave wave2"></div>
+		  <div class="wave wave3"></div>
+		  <div class="wave wave4"></div>
+          <p style="-webkit-background-clip: text; -webkit-text-fill-color: rgba(255, 255, 255, 01);">Contatos Abaixo</p>
+    </section>
+	  
+	  <div class="container">
+        <ul>
+          <li>
+            <a href="https://www.instagram.com/_fxlipeant/">
+               <span></span>
+               <span></span>
+               <span></span>
+               <span></span>
+               <span class="fa fa-instagram" aria-hidden="true"></span>
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/_FelipeFT">
+               <span></span>
+               <span></span>
+               <span></span>
+               <span></span>
+               <span class="fa fa-twitter" aria-hidden="true"></span>
+            </a>
+          </li>
+        </ul>
+    </div>
+	  
+        
+	  
+	    <footer class="footer mt-auto py-3">
+            <div class="text-center">
+          		<span class="text-muted">©2020, Antonio Felipe - Codi in <a target="_blank" href="https://github.com/FelipeAnt/CRUDphp">GitHub</a> </span>
+       		</div>
+        </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+    <script src="js/jquery-3.3.1.min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed --> 
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap-4.3.1.js"></script>
+  </body>
+</html>
